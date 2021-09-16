675b472ee03c00279e581b1c7deda4d161d1fcfa
Rishi Kumar <rishi3pb2@gmail.com>
2021-09-16 20:03:09 +0530

    better option lol

--- options.js
+++ options.js
@@ -2,7 +2,7 @@ export const initOptions = [
   "Create Express and React app",
   "Create Express and React app with Mongodb as database",
   "Create Express and React app with Sequelize as database",
-  "Create Express and React jwt auth app",
-  "Create Express and React jwt auth app with Mongodb as database",
-  "Create Express and React jwt auth app with Sequelize as database",
+  "Create Express and React app with auth built in",
+  "Create Express and React app with auth built in with Mongodb as database",
+  "Create Express and React app with auth built in with Sequelize as database",
 ];
