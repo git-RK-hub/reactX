1c13cde13a2cb3684a6983292703d1c6c22c2c7f
Rishi Kumar <rishi3pb2@gmail.com>
2021-10-29 23:02:56 +0530

    adding frontend files

--- app/frontend/index.js
+++ app/frontend/index.js
@@ -3,8 +3,15 @@ import path from 'path';
 import ora from 'ora';
 import chalk from 'chalk';
 import execa from 'execa';
+import app from './template/src/App.js';
+import appCss from './template/src/App.css.js';
+import index from './template/src/index.js';
+import indexCss from './template/src/index.css.js';
+import appTests from './template/src/App.test.js';
+import reportWebVitals from './template/src/reportWebVitals.js';
+import packageJSON from './template/package.json.js';
 
-const writeFrontend = async () => {
+const writeFrontend = async (options) => {
   // create a frontend folder
   const loader = ora('Writing frontend');
 
@@ -16,6 +23,104 @@ const writeFrontend = async () => {
     loader.succeed();
   } catch (err) {
     console.log(chalk.redBright('Error 💥', err));
+    process.exit(1);
+  }
+
+  // creating public folder
+  try {
+    await fs.mkdir(`${frontendPath}/public`);
+  } catch (err) {
+    console.log(chalk.redBright('Error 💥', err));
+    process.exit(1);
+  }
+
+  // creating src folder
+  try {
+    await fs.mkdir(`${frontendPath}/src`);
+  } catch (err) {
+    console.log(chalk.redBright('Error 💥', err));
+    process.exit(1);
+  }
+
+  // Writing index.js file
+  const indexFilePath = path.join(`${frontendPath}/src`, 'index.js');
+  try {
+    await fs.writeFile(indexFilePath, index());
+  } catch (err) {
+    console.log(chalk.redBright('Error 💥', err));
+    process.exit(1);
+  }
+
+  // Writing index.css file
+  const indexCssFilePath = path.join(`${frontendPath}/src`, 'index.css');
+  try {
+    await fs.writeFile(indexCssFilePath, indexCss());
+  } catch (err) {
+    console.log(chalk.redBright('Error 💥', err));
+    process.exit(1);
+  }
+
+  // Writing app.js file
+  const appFilePath = path.join(`${frontendPath}/src`, 'App.js');
+  try {
+    await fs.writeFile(appFilePath, app());
+  } catch (err) {
+    console.log(chalk.redBright('Error 💥', err));
+    process.exit(1);
+  }
+
+  // Writing App.css file
+  const appCssFilePath = path.join(`${frontendPath}/src`, 'App.css');
+  try {
+    await fs.writeFile(appCssFilePath, appCss());
+  } catch (err) {
+    console.log(chalk.redBright('Error 💥', err));
+    process.exit(1);
+  }
+
+  // Writing App.tests.js file
+  const appTestsFilePath = path.join(`${frontendPath}/src`, 'App.test.js');
+  try {
+    await fs.writeFile(appTestsFilePath, appTests());
+  } catch (err) {
+    console.log(chalk.redBright('Error 💥', err));
+    process.exit(1);
+  }
+
+  // Writing reportWebVitals.js file
+  const reportWebVitalsFilePath = path.join(
+    `${frontendPath}/src`,
+    'reportWebVitals.js'
+  );
+  try {
+    await fs.writeFile(reportWebVitalsFilePath, reportWebVitals());
+  } catch (err) {
+    console.log(chalk.redBright('Error 💥', err));
+    process.exit(1);
+  }
+
+  // write packagejson file and install dependencies
+  const packageJsonFilePath = path.join(frontendPath, 'package.json');
+
+  try {
+    await fs.writeFile(packageJsonFilePath, packageJSON(options.projectName));
+  } catch (err) {
+    console.log(chalk.redBright('Error 💥', err));
+    process.exit(1);
+  }
+
+  try {
+    loader.start();
+    loader.text = 'Installing frontend dependencies';
+    await execa('npm', ['install'], {
+      cwd: `./frontend`,
+      stdin: 'ignore',
+    });
+    loader.succeed();
+  } catch (err) {
+    loader.succeed();
+    console.log(chalk.redBright('Error 💥', err));
+    process.exit(1);
   }
 };
 
