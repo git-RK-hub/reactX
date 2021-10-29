0d8864cd7a232c4503bc577f7871b1e662bdf0e3
Rishi Kumar <rishi3pb2@gmail.com>
2021-10-29 22:16:07 +0530

    start creating fronted

--- app/frontend/index.js
+++ app/frontend/index.js
@@ -1,3 +1,22 @@
-const writeFrontend = () => {};
+import fs from 'fs-extra';
+import path from 'path';
+import ora from 'ora';
+import chalk from 'chalk';
+import execa from 'execa';
+
+const writeFrontend = async () => {
+  // create a frontend folder
+  const loader = ora('Writing frontend');
+
+  loader.start();
+
+  const frontendPath = `${process.cwd()}/frontend`;
+  try {
+    await fs.mkdir(frontendPath);
+    loader.succeed();
+  } catch (err) {
+    console.log(chalk.redBright('Error 💥', err));
+  }
+};
 
 export default writeFrontend;
